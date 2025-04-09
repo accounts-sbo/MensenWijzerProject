72c1295f700763fe3c5136bd51915610b1fd3ca7
mtwim <willemvanleunen@gmail.com>
2025-04-09 14:23:51 +0200

    Update Testimonials.tsx
    testimonials toegevoegd

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -56,6 +56,78 @@ const testimonials: TestimonialType[] = [
     role: "Directeur NLV",
     quote: "Sipke Jan heeft voor ons de hele campagne verzorgd; ik kon het loslaten met het vertrouwen dat het in orde zou komen. De kern van het verhaal wist Sipke Jan goed te raken. Hij weet waar het om draait en wat voor ons en onze achterban belangrijk is. De samenwerking verliep uitstekend en we weten elkaar nog steeds altijd te vinden voor nieuwe plannen.",
   }
+   {
+    id: 7,
+    name: "Halbe-Piter Claus",
+    role: "Regisseur / Programmaker",
+    quote: "Als projectadviseur van het transmediale filmproject Jennifer to the Max is Sipke Jan er in geslaagd verbindingen te leggen tussen verschillende maatschappelijke organisaties op nationaal niveau. Dit heeft geresulteerd in een stevige samenwerking waarbij vorm en inhoud elkaar versterken. Op een prettige manier verbindt Sipke Jan de verschillende doelstellingen van deelnemende partners. Een strategisch complexe klus die Sipke Jan op het lijf geschreven is.",
+  },
+  {
+    id: 8,
+    name: "Jan Bakker",
+    role: "Projectleider Gemeente Heerenveen",
+    quote: "Erg onder de indruk was ik van de presentatie coaching sessie die je voor de studenten hebt verzorgd. De deelnemende studententeams moesten tijdens het Light Challenge traject de voortgang van hun traject maar ook zichzelf presenteren aan de vakjury en het publiek. Tijdens de coaching sessie heb je de studenten kneepjes uit het vak maar ook zelfvertrouwen meegegeven waardoor ze een enorme ontwikkeling hebben mogen doormaken.",
+  },
+  {
+    id: 9,
+    name: "Jan Bakker",
+    role: "Projectleider Gemeente Heerenveen",
+    quote: "Vanuit de Light Challenge organisatie hebben we deze vierde editie de lat erg hoog gelegd. Jij hebt ons daarbij geholpen ons doel te realiseren. De Kick-off, masterclasses en finale die je als dagvoorzitter hebt verzorgd zijn door jou zeer professioneel uitgevoerd. Door je zakelijke insteek te combineren met luchtige zelfreflectie en een dosis humor heb je de juiste sfeer voor de Light Challenge weten te bepalen.",
+  },
+  {
+    id: 10,
+    name: "José Willemse",
+    role: "Directeur NLV",
+    quote: "Werken met Sipke Jan is inspirerend. Hij bundelt je ideeën, vertaalt ze naar de mogelijkheden die er zijn. Het is een groot genoegen om samen met hem te werken. Hij is deskundig op zijn vakgebied en verdiept zich vol verve in jouw onderwerp. Dat maakt dat hij precies weet waar we samen naar toe kunnen. Met Sipke Jan hebben wij het beste in onszelf naar boven gehaald.",
+  },
+  {
+    id: 11,
+    name: "Trudy Feijen",
+    role: "Communicatie- en gedragsexpert",
+    quote: "Sipke Jan Bousema en zijn team durven een duidelijk standpunt in te nemen en weten als geen ander hoe je de identiteit, waarden en normen van een organisatie vertaalt.",
+  },
+  {
+    id: 12,
+    name: "Trudy Feijen",
+    role: "",
+    quote: "Vanuit authenticiteit weet Sipke Jan mensen te inspireren. Zijn passie, daadkracht en empathisch vermogen maken dat hij de kern weet te raken. Een vakman die in staat is om ook anderen naar een next level te trainen. Daarnaast is hij een zéér prettige persoonlijkheid om mee samen te werken.",
+  },
+  {
+    id: 13,
+    name: "Sytse Oreel",
+    role: "Directeur Oreel Metal Components & Assemblies",
+    quote: "Sipke Jan Bousema kan goed inschatten of een idee hout snijdt of niet. Hij heeft daarnaast een goed gevoel voor timing, waardoor de campagne door veel media is opgepikt. Zowel door de lokale en regionale media, als landelijke media zoals BNR Nieuwsradio. Dit laatste was meer dan waar we op hoopten.",
+  },
+  {
+    id: 14,
+    name: "Eva Meijer",
+    role: "Directeur Hoora Watersport",
+    quote: "Ons bedrijf is echt weer goed op de kaart gezet en onze missie om goed, passend personeel te vinden is meer dan geslaagd!",
+  },
+  {
+    id: 15,
+    name: "Eva Meijer",
+    role: "Directeur Hoora Watersport",
+    quote: "Er is dankzij de campagne meer bereikt dan ik had durven dromen! We hebben onwijs goed in de spotlight gestaan.",
+  },
+  {
+    id: 16,
+    name: "Fokke Jagersma",
+    role: "Projectcoördinator AFÛK",
+    quote: "De samenwerking met Sipke Jan Bousema heb ik als plezierig, warm en enthousiast ervaren. Hij heeft geleid tot een verrassende en creatieve site die de doelgroep aansprak.",
+  },
+  {
+    id: 17,
+    name: "Prof. Dr. Solko Schalm",
+    role: "Leverarts",
+    quote: "Ik ben enorm onder de indruk van hoe verbindend Sipke Jan kan werken en van zijn professionele aanpak.",
+  },
+  {
+    id: 18,
+    name: "Jeannette Verbruggen",
+    role: "Directie Samenleving & Integratie van het Ministerie van Sociale Zaken & Werkgelegenheid",
+    quote: "De netwerkbijeenkomst van de directie Samenleving & Integratie van het ministerie van SZW is een heel groot succes geworden. Mede dankzij de bijdrage van Sipke Jan Bousema zowel bij het voortraject als op de dag zelf in zijn rol als dagvoorzitter.",
+  }
 ];
 
 const Testimonials = () => {
